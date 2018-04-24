c60af0bf024671aa381f2ef90a82d01808f33f5c
HvrlK <g.v.s.23.07@gmail.com>
2018-04-24 20:16:21 +0300

    Add search bar and table view.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResults.swift
+//  StoreSearch
+//
+//  Created by Vitalii Havryliuk on 4/24/18.
+//  Copyright © 2018 Vitalii Havryliuk. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
