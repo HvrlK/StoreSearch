64c55e325d264290d02c2a4813ad3dfa07b61ade
HvrlK <g.v.s.23.07@gmail.com>
2018-04-27 15:58:15 +0300

    Add pop-up view.

---
+++ StoreSearch/DetailViewController.swift
@@ -0,0 +1,112 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Vitalii Havryliuk on 4/27/18.
+//  Copyright © 2018 Vitalii Havryliuk. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var searchResult: SearchResult!
+    var downloadTask: URLSessionDownloadTask?
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
+    
+    // MARK: - Methods
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        modalPresentationStyle = .custom
+        transitioningDelegate = self
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        popupView.layer.cornerRadius = 10
+        let gestureRecignizer = UITapGestureRecognizer(target: self, action: #selector(close))
+        gestureRecignizer.cancelsTouchesInView = false
+        gestureRecignizer.delegate = self
+        view.addGestureRecognizer(gestureRecignizer)
+        if searchResult != nil {
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        nameLabel.text = searchResult.name
+        if searchResult.artistName.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = searchResult.artistName
+        }
+        kindLabel.text = searchResult.kindForDisplay()
+        genreLabel.text = searchResult.genre
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = searchResult.currency
+        
+        let priceText: String
+        if searchResult.price == 0 {
+            priceText = "Free"
+        } else if let text = formatter.string(from: searchResult.price as NSNumber) {
+            priceText = text
+        } else {
+            priceText = ""
+        }
+        priceButton.setTitle(priceText, for: .normal)
+        if let largeURL = URL(string: searchResult.artworkLargeURL) {
+            downloadTask = artworkImageView.loadImage(url: largeURL)
+        }
+    }
+    
+    deinit {
+        print("deinit \(self)")
+        downloadTask?.cancel()
+    }
+
+    // MARK: Actions
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func openInStore() {
+        if let url = URL(string: searchResult.storeURL) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+
+}
+
+// MARK: - Extensions
+
+extension DetailViewController: UIViewControllerTransitioningDelegate {
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+}
+
+extension DetailViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return (touch.view === self.view)
+    }
+    
+}
---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,19 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Vitalii Havryliuk on 4/27/18.
+//  Copyright © 2018 Vitalii Havryliuk. All rights reserved.
+//
+
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+    
+    // MARK: - Methods
+    
+    override var shouldRemovePresentersView: Bool {
+        return false
+    }
+    
+}
